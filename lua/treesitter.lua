b52a27daf99a69896d0ef7f5ebb48610137bbeb2
José Victor Ferreira <josevictorferreira@outlook.com>
2021-11-16 23:54:26 -0300

    enhanced treesiter highlighting

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,10 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
-    custom_captures = {
-      ["foo.bar"] = "Identifier",
-    },
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = true,
   },
   indent = {
     enable = true
