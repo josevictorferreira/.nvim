2f6c69531f47594dc56274fc8fbe9205be8e1315
Jose Victor <josevictorferreira@outlook.com>
2022-04-11 19:56:57 -0300

    added bash lsp dependency install

--- dependencies.sh
+++ dependencies.sh
@@ -9,4 +9,5 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   pacaur -S lua-language-server-git
   pacaur -S volar-server
   pacaur -S efm-langserver
+  pacaur -S bash-language-server
 fi
