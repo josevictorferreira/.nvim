7ef6510df609a53f8fe3bc4c7f5cd6cab74b63b7
Jose Victor <josevictorferreira@outlook.com>
2022-05-31 20:05:07 -0300

    updated snippet plugin and added ruby snippet

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -106,10 +106,10 @@ return require('packer').startup({function()
   use {
     'L3MON4D3/LuaSnip',
     requires = {
-      'rafamadriz/friendly-snippets'
+      'honza/vim-snippets'
     },
     config = function ()
-      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_snipmate").lazy_load()
       require'luasnip'.filetype_extend("ruby", {"rails"})
     end
   }
