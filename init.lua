714bde96dd651a478dacb6e821bb88195e0f24d9
Jose Victor <josevictorferreira@outlook.com>
2023-01-15 22:08:09 -0300

    changed plugin manager to lazy

--- init.lua
+++ init.lua
@@ -1,4 +1,22 @@
-require("plugins")
+--- Setup Lazy Neovim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+--- Map Leader Key to <space>
+vim.g.mapleader = ' '
+vim.opt.termguicolors = true
+
+require("lazy").setup("plugins")
 require("impatient")
 require("functions")
 require("mappings")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,18 +1,11 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
-end
-
-return require('packer').startup({function()
+return {
   -- Essential setup plugins
-  use({
+  {
     'wbthomason/packer.nvim',
     'lewis6991/impatient.nvim',
     'nvim-lua/plenary.nvim',
-  })
-  use {
+  },
+  {
     'nathom/filetype.nvim',
     config = function ()
       require("filetype").setup({
@@ -23,49 +16,49 @@ return require('packer').startup({function()
         }
       })
     end
-  }
+  },
   -- Crystal language support
-  use "vim-crystal/vim-crystal"
+  { "vim-crystal/vim-crystal" },
   -- Telescope Navigation
-  use {
+  {
     'nvim-telescope/telescope.nvim',
-    requires = {
-      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    dependencies = {
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function ()
       require('navigation')
     end
-  }
+  },
   -- Autopair
-  use {
+  {
     'windwp/nvim-autopairs',
     config = function ()
       require("autopairs")
     end
-  }
+  },
   -- Git Signs
-  use {
+  {
     'lewis6991/gitsigns.nvim',
     config = function()
       require('gitsigns').setup()
     end
-  }
+  },
   -- Treesitter
-  use {
+  {
     'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    requires = {
-      { 'nvim-treesitter/nvim-treesitter-refactor', opt = true },
-      { 'windwp/nvim-ts-autotag', opt = true }
+    build = ':TSUpdate',
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter-refactor', lazy = true },
+      { 'windwp/nvim-ts-autotag', lazy = true }
     },
     config = function ()
       require('treesitter')
     end
-  }
+  },
   -- Lsp Integration and Completion
-  use {
+  {
     'neovim/nvim-lspconfig',
-    requires = {
+    dependencies = {
       { 'ray-x/lsp_signature.nvim' },
       { 'williamboman/nvim-lsp-installer' },
       { 'hrsh7th/nvim-cmp' },
@@ -85,98 +78,98 @@ return require('packer').startup({function()
         }
       })
     end
-  }
+  },
 
   -- Multiple cursors
-  use 'terryma/vim-multiple-cursors'
+  { 'terryma/vim-multiple-cursors' },
 
   -- Status Bar plugin
-  use {
+  {
     'nvim-lualine/lualine.nvim',
     config = function ()
       require("statusbar")
     end
-  }
+  },
   -- DevIcons plugin
-  use 'kyazdani42/nvim-web-devicons'
+  { 'kyazdani42/nvim-web-devicons' },
   -- Colorize hex colors
-  use {
+  {
     'norcalli/nvim-colorizer.lua',
     config = function ()
       require("colorizer").setup()
     end
-  }
+  },
   -- Comment plugin
-  use {
+  {
     'numToStr/Comment.nvim',
     config = function ()
       require("Comment").setup()
     end
-  }
+  },
   -- Buffer Tabs
-  use {
+  {
     'akinsho/bufferline.nvim',
     config = function ()
       require("bufferline").setup{}
     end
-  }
+  },
   -- Snippets
-  use {
+  {
     'L3MON4D3/LuaSnip',
-    requires = {
+    dependencies = {
       'honza/vim-snippets'
     },
     config = function ()
       require("luasnip.loaders.from_snipmate").lazy_load()
       require'luasnip'.filetype_extend("ruby", {"rails"})
     end
-  }
+  },
   -- Nerdtree file sidebar
-  use {
+  {
     'kyazdani42/nvim-tree.lua',
     config = function ()
       require("file_explorer")
     end
-  }
+  },
   -- Debug startuptime
-  use 'dstein64/vim-startuptime'
+  { 'dstein64/vim-startuptime' },
   -- Colorscheme Plugin
-  use({
+  {
     "catppuccin/nvim",
-    as = "catppuccin",
+    name = "catppuccin",
     config = function ()
       require("colorscheme")
     end
-  })
+  },
   -- Disable relative line numbers in insert mode
-  use {
+  {
     'nkakouros-original/numbers.nvim',
     config = function ()
       require('numbers').setup()
     end
-  }
+  },
   -- Better Markdown
-  use {"ellisonleao/glow.nvim", branch = 'main'}
+  { "ellisonleao/glow.nvim", branch = 'main' },
   -- Motion
-  use {
+  {
     'phaazon/hop.nvim',
     branch = 'v2', -- optional but strongly recommended
     config = function()
       -- you can configure Hop the way you like here; see :h hop-config
       require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
     end
-  }
+  },
   -- Tests
-  use {
+  {
     "klen/nvim-test",
     config = function()
       require('nvim-test').setup()
     end
-  }
+  },
   -- Managing external editor tooling
-  use {
+  {
     'williamboman/mason.nvim',
-    requires = {
+    dependencies = {
       "williamboman/mason-lspconfig.nvim"
     },
     config = function ()
@@ -194,14 +187,4 @@ return require('packer').startup({function()
       })
     end
   }
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end,
-config = {
-  profile = {
-    enable = true,
-    threshold = 1 -- the amount in ms that a plugins load time must be over for it to be included in the profile
-  }
-}}
-)
+}
