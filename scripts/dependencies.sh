fbdc1de20bd631d3f4836d3a0f22ac520d1821d6
Jose Victor Ferreira <josevictorferreira@outlook.com>
2023-01-23 23:57:47 -0300

    removed dependencies lsp

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -7,9 +7,4 @@ if [ "$(uname)" == "Darwin" ]; then
 	brew install ripgrep
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	pacaur -S ripgrep
-  pacaur -S lua-language-server-git
-  pacaur -S volar-server
-  pacaur -S efm-langserver
-  pacaur -S bash-language-server
-  pacaur -S gopls
 fi
