a60c61348b272695db722508c2929df715b55adf
Jose Victor <josevictorferreira@outlook.com>
2023-03-17 23:35:04 -0300

    removed yaml schemas

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -51,8 +51,6 @@ lspconfig.yamlls.setup {
     yaml = {
       schemas = {
         ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-        ["../path/relative/to/file.yml"] = "/.github/workflows/*",
-        ["/path/from/root/of/project"] = "/.github/workflows/*",
         ['https://raw.githubusercontent.com/docker/cli/master/cli/compose/schema/data/config_schema_v3.9.json'] = '/docker-compose.yml',
         ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml"
       },
