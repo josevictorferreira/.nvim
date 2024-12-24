14e38621e8228c0bcefc2e8b12c310c9644c1b92
Jose Victor Ferreira <josevictorferreira@outlook.com>
2024-12-24 11:43:57 -0300

    init repostiroy

---
+++ hosts/josevictor-nixos/variables.nix
@@ -0,0 +1,16 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Jose Victor Ferreira";
+  gitEmail = "josevictorferreira@outlook.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "alacritty"; # Set Default System Terminal
+  keyboardLayout = "us";
+}
