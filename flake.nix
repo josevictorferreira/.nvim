8154ee116a0abbe35ebc92fb6828b660626a151d
Jose Victor Ferreira <josevictorferreira@outlook.com>
2025-04-11 11:46:15 -0300

    added darwin support in macos

--- flake.nix
+++ flake.nix
@@ -3,9 +3,14 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-darwin.url = "nixpkgs/nixpkgs-unstable";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes";
     sops-nix.url = "github:Mic92/sops-nix";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,30 +18,37 @@
   };
 
   outputs =
-    inputs@{ self, nixpkgs, sops-nix, home-manager, ... }:
+    inputs@{ self, nixpkgs, nixpkgs-darwin, darwin, sops-nix, home-manager, ... }:
     let
-      system = "x86_64-linux";
-      host = "josevictor-nixos";
-      username = "josevictor";
-
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-        };
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (
+        import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        }
       };
+      darwinPkgsFor = system: import nixpkgs-darwin {
+        inherit system;
+        config = { allowUnfree = true; };
+      }; 
+      system = "x86_64-linux";
+      nixHost = "josevictor-nixos";
+      macosHost = "josevictorferreira-macos";
+      username = "josevictor";
     in
     {
       nixosConfigurations = {
-        "${host}" = nixpkgs.lib.nixosSystem rec {
+        "${nixHost}" = nixpkgs.lib.nixosSystem rec {
           specialArgs = {
             inherit system;
             inherit inputs;
             inherit username;
-            inherit host;
+            host = nixHost;
+            pkgs = nixpkgsFor.${system};
           };
           modules = [
-            ./hosts/${host}/config.nix
+            ./hosts/${nixHost}/config.nix
             inputs.distro-grub-themes.nixosModules.${system}.default
             sops-nix.nixosModules.sops
 
@@ -45,8 +57,45 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = specialArgs;
+              home-manager.extraSpecialArgs = {
+                inherit system;
+                inherit inputs;
+                inherit username;
+                host = nixHost;
+                isNixOS = true;
+                isDarwin = false;
+              };
               home-manager.users.${username} = import ./home/${username}/nixos.nix;
+              home-manager.backupFileExtension = "backup";
+            }
+          ];
+        };
+      };
+
+      darwinConfigurations = {
+        "${macosHost}" = darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          specialArgs = {
+            inherit inputs;
+            inherit username;
+            host = macosHost;
+            pkgs = darwinPkgsFor "aarch64-darwin";
+          };
+          modules = [
+            ./hosts/${macosHost}/darwin.nix
+            
+            # Home Manager for macOS
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = {
+                inherit inputs username;
+                host = macosHost;
+                isNixOS = false;
+                isDarwin = true;
+              };
+              home-manager.users.${username} = import ./home/${username}/macos.nix;
               home-manager.backupFileExtension = "backup";
             }
           ];
