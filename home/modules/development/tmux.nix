9d01f8dbed692aec3e9724088ef564e515f77d9f
Jose Victor Ferreira <josevictorferreira@outlook.com>
2025-04-10 23:41:42 -0300

    updated tmux config

--- home/modules/development/tmux.nix
+++ home/modules/development/tmux.nix
@@ -6,16 +6,37 @@ in
 {
   programs.tmux = {
     enable = true;
-    extraConfig = builtins.readFile "${tmuxConfigDir}/tmux.conf";
     plugins = with pkgs.tmuxPlugins; [
-      sensible
       yank
+      sensible
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-boot 'on'
+          set -g @continuum-save-interval 'on'
+        '';
+      }
+      {
+        plugin = tokyo-night-tmux;
+        extraConfig = ''
+          set -g @tokyo-night-tmux_theme storm    # storm | day | default to 'night'
+          set -g @tokyo-night-tmux_transparent 1  # 1 or 0
+          set -g @tokyo-night-tmux_window_id_style digital
+          set -g @tokyo-night-tmux_pane_id_style hsquare
+          set -g @tokyo-night-tmux_zoom_id_style dsquare
+        '';
+      }
     ];
-    terminal = "tmux-256color";
-    escapeTime = 10;
-    historyLimit = 10000;
-  };
-  home.file = {
-    ".config/tmux".source = "${tmuxConfigDir}";
+
+    extraConfig = builtins.readFile "${tmuxConfigDir}/tmux.conf";
   };
 }
