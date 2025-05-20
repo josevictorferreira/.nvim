501ce92c31f8d304e3b5872a31e415f31ce0bb5e
Jose Victor Ferreira <root@josevictor.me>
2025-05-19 21:41:21 -0300

    updated keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,13 +2,6 @@ vim.g.mapleader = " "
 
 local options = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
-local nmap = function(keys, func, desc)
-	if desc then
-		desc = "LSP: " .. desc
-	end
-
-	vim.keymap.set("n", keys, func, options)
-end
 
 map("v", "J", ":m '>+1<CR>gv=gv", options)
 map("v", "K", ":m '<-2<CR>gv=gv", options)
@@ -28,17 +21,37 @@ map("n", "<leader>sl", ":rightbelow vsplit<CR>", options)
 map("n", "<leader>sk", ":leftabove split<CR>", options)
 map("n", "<leader>sj", ":rightbelow split<CR>", options)
 
-map("n", "<C-j>", "<C-w>j", options)
-map("n", "<C-k>", "<C-w>k", options)
-map("n", "<C-l>", "<C-w>l", options)
-map("n", "<C-h>", "<C-w>h", options)
+map("n", "<C-w>H", ":vertical resize -5<CR>", options)
+map("n", "<C-w>L", ":vertical resize +5<CR>", options)
+map("n", "<C-w>K", ":resize -2<CR>", options)
+map("n", "<C-w>J", ":resize +2<CR>", options)
 
 map("n", "<leader>,", ":noh<CR>", options)
-nmap("gd", vim.lsp.buf.definition)
-nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
 
-map("n", "<leader>jl", ":%!jq .<CR>", options)
+vim.keymap.set("n", "<leader>r", function()
+	print("Resize mode: h/l = width, j/k = height. Press q to quit.")
+	local resize_keys = {
+		h = ":vertical resize -2<CR>",
+		l = ":vertical resize +2<CR>",
+		j = ":resize -2<CR>",
+		k = ":resize +2<CR>",
+		q = nil,
+	}
 
-map("v", "<leader>r", ":lua ReplaceAllFromSelectedText()<CR>", options)
+	local function resize_loop()
+		vim.fn.getcharstr()
+		local key = vim.fn.nr2char(vim.fn.getchar(0))
+		print("Key pressed: " .. key)
+		local cmd = resize_keys[key]
+		if key == "q" then
+			print("Exiting resize mode.")
+			return
+		end
+		if cmd then
+			vim.cmd(cmd)
+			resize_loop()
+		end
+	end
 
-map("v", "<leader>rp", ":lua ProjectSearchAndReplace()<CR>", options)
+	resize_loop()
+end)
