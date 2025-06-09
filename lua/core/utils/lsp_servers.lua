6e9c5ddd943935302e853edeb50bf7c0d40821d4
Jose Victor Ferreira <root@josevictor.me>
2025-06-09 12:01:40 -0300

    updated some lsp servers

--- lua/core/utils/lsp_servers.lua
+++ lua/core/utils/lsp_servers.lua
@@ -1,70 +1,71 @@
 return {
-	rust_analyzer = { filetypes = { "rs", "rust" } },
-	bashls = { filetypes = { "sh", "bash" } },
-	cmake = { filetypes = { "cmake" } },
-	cssls = { filetypes = { "css" } },
-	dockerls = { filetypes = { "Dockerfile", "dockerfile" } },
-	docker_compose_language_service = { filetypes = { "yaml.docker-compose", "docker-compose" } },
-	html = { filetypes = { "html" } },
-	jsonls = { filetypes = { "json" } },
-	ruff = { filetypes = { "python" } },
-	vimls = { filetypes = { "vim" } },
-	yamlls = {
-		filetypes = { "yaml", "yml" },
-		settings = {
-			yaml = {
-				schemas = {
-					["https://raw.githubusercontent.com/Kong/go-database-reconciler/refs/heads/main/pkg/file/kong_json_schema.json"] = {
-						"kong*.yml",
-						"kong*.yaml",
-					},
-				},
-			},
-		},
-	},
-	gleam = { filetypes = { "gleam" }, auto_install = false },
-	dotls = { filetypes = { "dot" } },
-	jdtls = { filetypes = { "java" } },
-	crystalline = { filetypes = { "cr" } },
-	tailwindcss = {
-		filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact", "vue", "typescript.tsx" },
-	},
-	ruby_lsp = {
-		filetypes = { "ruby" },
-		cmd = { "bundle", "exec", "ruby-lsp", "stdio" },
-		init_options = {
-			formatter = "rubocop",
-			linter = "rubocop",
-		},
-		auto_install = false,
-	},
-	sorbet = {
-		filetypes = { "ruby" },
-		root_markers = { "Gemfile", ".git", "sorbet" },
-		cmd = { "bundle", "exec", "srb", "tc", "--lsp" },
-		auto_install = false,
-	},
-	buf_ls = { filetypes = { "proto", "protobuf" } },
-	lua_ls = { filetypes = { "lua" }, settings = { Lua = { diagnostics = { globals = { "vim", "use" } } } } },
-	eslint = { filetypes = { "javascript", "vue", "typescript", "typescriptreact", "typescript.tsx", "tsx" } },
-	ts_ls = { filetypes = { "typescript", "javascript", "jsx", "tsx", "typescriptreact" } },
-	emmet_ls = { filetypes = { "vue", "html" } },
-	nixd = { filetypes = { "nix" }, auto_install = false },
-	helm_ls = {
-		filetypes = { "helm" },
-		settings = {
-			["helm-ls"] = {
-				yamlls = {
-					path = "yaml-language-server",
-					config = {
-						schemas = {
-							[vim.fn.stdpath("config") .. "/schemas/helmfile.json"] = {
-								"helmfile.yaml",
-							},
-						},
-					},
-				},
-			},
-		},
-	},
+  rust_analyzer = { filetypes = { "rs", "rust" } },
+  bashls = { filetypes = { "sh", "bash" } },
+  cmake = { filetypes = { "cmake" } },
+  autotools_ls = { filetypes = { "config", "automake", "make", "Makefile" } },
+  cssls = { filetypes = { "css" } },
+  dockerls = { filetypes = { "Dockerfile", "dockerfile" } },
+  docker_compose_language_service = { filetypes = { "yaml.docker-compose", "docker-compose" } },
+  html = { filetypes = { "html" } },
+  jsonls = { filetypes = { "json" } },
+  ruff = { filetypes = { "python" } },
+  vimls = { filetypes = { "vim" } },
+  yamlls = {
+    filetypes = { "yaml", "yml" },
+    settings = {
+      yaml = {
+        schemas = {
+          ["https://raw.githubusercontent.com/Kong/go-database-reconciler/refs/heads/main/pkg/file/kong_json_schema.json"] = {
+            "kong*.yml",
+            "kong*.yaml",
+          },
+        },
+      },
+    },
+  },
+  gleam = { filetypes = { "gleam" }, auto_install = false },
+  dotls = { filetypes = { "dot" } },
+  jdtls = { filetypes = { "java" } },
+  crystalline = { filetypes = { "cr" } },
+  tailwindcss = {
+    filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact", "vue", "typescript.tsx" },
+  },
+  ruby_lsp = {
+    filetypes = { "ruby" },
+    cmd = { "bundle", "exec", "ruby-lsp", "stdio" },
+    init_options = {
+      formatter = "rubocop",
+      linter = "rubocop",
+    },
+    auto_install = false,
+  },
+  sorbet = {
+    filetypes = { "ruby" },
+    root_markers = { "Gemfile", ".git", "sorbet" },
+    cmd = { "bundle", "exec", "srb", "tc", "--lsp" },
+    auto_install = false,
+  },
+  protols = { filetypes = { "proto", "protobuf" } },
+  lua_ls = { filetypes = { "lua" }, settings = { Lua = { diagnostics = { globals = { "vim", "use" } } } } },
+  eslint = { filetypes = { "javascript", "vue", "typescript", "typescriptreact", "typescript.tsx", "tsx" } },
+  ts_ls = { filetypes = { "typescript", "javascript", "jsx", "tsx", "typescriptreact" } },
+  emmet_ls = { filetypes = { "vue", "html" } },
+  nixd = { filetypes = { "nix" }, auto_install = false },
+  helm_ls = {
+    filetypes = { "helm" },
+    settings = {
+      ["helm-ls"] = {
+        yamlls = {
+          path = "yaml-language-server",
+          config = {
+            schemas = {
+              [vim.fn.stdpath("config") .. "/schemas/helmfile.json"] = {
+                "helmfile.yaml",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
