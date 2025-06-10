885af78109f471e722c4a50c4b3810f4af23d3d3
Jose Victor Ferreira <root@josevictor.me>
2025-06-09 22:08:34 -0300

    updated telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,22 +5,23 @@ local set_telescope_keymap = function()
 	map("n", "<leader>p", ":Telescope find_files<CR>", options)
 	map("n", "<leader>fg", ":Telescope live_grep<CR>", options)
 	map("n", "<leader>f", ":Telescope live_grep<CR>", options)
-	map("n", "<leader>fb", ":Telescope buffers<CR>", options)
+	map("n", "<leader>b", ":Telescope buffers<CR>", options)
 	map("n", "<leader>fh", ":Telescope help_tags<CR>", options)
 end
 
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
+		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-telescope/telescope-ui-select.nvim",
 	},
+	tag = "0.1.8",
 	config = function()
 		require("telescope").setup({
 			defaults = {
 				file_ignore_patterns = { "node_modules", "target", ".git", ".cache" },
 				set_env = { ["COLORTERM"] = "truecolor" },
-				layout_strategy = "horizontal",
+				layout_strategy = "vertical",
 				initial_mode = "insert",
 				selection_strategy = "reset",
 				sorting_strategy = "ascending",
@@ -51,14 +52,10 @@ return {
 			extensions = {
 				wrap_results = true,
 				fzf = {},
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown({}),
-				},
 			},
 		})
 
 		pcall(require("telescope").load_extension, "fzf")
-		pcall(require("telescope").load_extension, "ui-select")
 
 		set_telescope_keymap()
 	end,
