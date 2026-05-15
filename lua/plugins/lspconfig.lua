6b8c79d7e01052904c1db557b820f42a39bdbd1b
Jose Victor Ferreira <root@josevictor.me>
2026-05-15 13:48:48 -0300

    feat: Remove blink.cmp and enable inline completion
    The `blink.cmp` plugin has been removed. This plugin was previously used to enhance the LSP client capabilities and provide completion UI.
    
    The change was made to simplify the configuration and leverage Neovim's built-in inline completion feature. The `vim.lsp.inline_completion.enable()` function is now used to activate this functionality, and a keymap for `<Tab>` has been added to accept the inline completion when available. This streamlines the completion experience and reduces reliance on external plugins for this feature.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -33,7 +33,7 @@ local function setup_lsp_servers(servers)
 	for server, config in pairs(servers) do
 		local success, _ = pcall(function()
 			config.on_attach = on_attach
-			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			config.capabilities = vim.lsp.protocol.make_client_capabilities()
 			vim.lsp.config(server, config)
 			vim.lsp.enable(server)
 		end)
@@ -45,9 +45,15 @@ end
 
 return {
 	"neovim/nvim-lspconfig",
-	dependencies = { "saghen/blink.cmp" },
-	event = { "BufReadPre", "BufNewFile" },
 	config = function()
+		vim.lsp.inline_completion.enable()
+
+		vim.keymap.set("i", "<Tab>", function()
+			if not vim.lsp.inline_completion.get() then
+				return "<Tab>"
+			end
+		end, { expr = true, desc = "Accept the current inline completion" })
+
 		local lsp_servers = require("core.utils.lsp_servers")
 
 		setup_lsp_servers(lsp_servers)
