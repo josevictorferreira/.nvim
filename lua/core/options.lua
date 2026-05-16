e5aae7f62fdb33545fde1380a7292de5f9ebfca0
Jose Victor Ferreira <root@josevictor.me>
2026-05-16 09:27:13 -0300

    feat: Enable fold expressions for better code folding
    This commit introduces the `foldmethod` and `foldexpr` options to use nvim-treesitter's fold expressions. This will provide more intelligent and context-aware code folding, improving code readability and navigation. The `foldlevel` is set to 99 to ensure folding is enabled by default for all files.

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -27,6 +27,10 @@ vim.opt.syntax = "enable"
 
 vim.opt.guicursor = ""
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
+
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
